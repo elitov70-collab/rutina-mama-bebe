47f3880fd9209406f66a04ab11fd41ff73585f2a
elitov70-collab <elitov70@gmail.com>
2026-07-12 17:09:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const saludo = document.getElementById("saludo");
 
+const hora = new Date().getHours();
+
+if (hora < 12) {
+    saludo.textContent = "☀️ Buenos días";
+} else if (hora < 19) {
+    saludo.textContent = "🌤️ Buenas tardes";
+} else {
+    saludo.textContent = "🌙 Buenas noches";
+}
